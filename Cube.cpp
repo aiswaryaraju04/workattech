920cbf654f6dd45e29d74e660bddc7fa5818b193
Aiswarya Raju <aiswaryaraju2001@gmail.com>
2022-03-27 22:25:23 +0530

    New file

---
+++ Cube.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int a,sa,vol;
+	// your code goes here
+	cin>>a;
+	sa= 6*a*a;
+	vol=a*a*a;
+	
+	cout<<sa<<" "<<vol;
+	return 0;
+}
---
+++ SumUptoTen.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int sum=0;
+	for(int i=1;i<11;i++)
+	{
+		sum=sum+i;
+	}
+	cout<<sum;
+	// your code goes here
+	return 0;
+}
