c62b416c28ba52efc3121703506150b1b5572dcd
Aiswarya Raju <aiswaryaraju2001@gmail.com>
2022-03-27 22:50:48 +0530

    New file

---
+++ Candies.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int candies,cousins;
+	cin>>candies>>cousins;
+	if(candies%cousins==0)
+	{
+		cout<<"YES";
+	}
+	else
+	{
+		cout<<"NO";
+	}
+	// your code goes here
+	return 0;
+}
---
+++ SimpleInterest.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	double p,r,t;
+	cin>>p>>r>>t;
+	double interest;
+	interest=(p*r*t)/100;
+	cout<<fixed<<setprecision(6)<<interest;
+	// your code goes here
+	return 0;
+}
