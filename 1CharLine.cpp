87af8019cde926950579426b189aa24a4b750e6b
Aiswarya Raju <aiswaryaraju2001@gmail.com>
2022-05-03 21:47:14 +0530

    new file

---
+++ 1CharLine.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	string str="WORKATTECH";
+		for(int i=0;i<str.length();i++)
+			cout<<str.at(i)<<endl;
+	return 0;
+}
